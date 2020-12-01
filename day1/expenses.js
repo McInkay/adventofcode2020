a693c7173efd9f32304f34b2f0441dc319980546
Al McKinlay <alistair.mckinlay@symphonicsoft.com>
2020-12-01 16:26:53 +0000

    Add day 2 part 2

--- day1/expenses.js
+++ day1/expenses.js
@@ -9,7 +9,15 @@ const part1 = (data) => {
 }
 
 const part2 = (data) => {
-	return true;
+	for (const e1 of data) {
+		for (const e2 of data) {
+			for (const e3 of data) {
+				if (Number(e1) + Number(e2) + Number(e3) === 2020) {
+					return Number(e1) * Number(e2) * Number(e3);
+				}
+			}
+		}
+	}
 }
 
 module.exports = {
