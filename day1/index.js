55aaf5deab77fe08ec5b72e25e314ed78c8fa373
Al McKinlay <github@10people.co.uk>
2020-12-01 19:59:13 +0000

    Rename day 1

--- day1/index.js
+++ day1/index.js
@@ -1,6 +1,6 @@
 const expenses = require("./expenses");
 export const data = require("./data.txt");
 
-export const name = "Expenses`" ;
+export const name = "Expenses" ;
 export const part1 = (data) => expenses.part1(data.split("\n"));
 export const part2 = (data) => expenses.part2(data.split("\n"));
