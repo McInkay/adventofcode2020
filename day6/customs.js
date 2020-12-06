3580692a1377ae7ac9281796b092124126319863
Al McKinlay <github@10people.co.uk>
2020-12-06 20:12:43 +0000

    Day 6, part 2

--- day6/customs.js
+++ day6/customs.js
@@ -4,13 +4,25 @@ const part1 = (groups) => {
 		.reduce((total, num) => total + num);
 }
 
-const part2 = (lines) => {
-}
-
 const getNumberOfYes = (group) => {
 	const qs = new Set();
 	group.forEach((person) => person.split("").forEach((q) => qs.add(q)));
 	return qs.size;
+}
+
+const part2 = (groups) => {
+	return groups
+		.map((group) => getNumberOfAll(group.split("\n")))
+		.reduce((total, num) => total + num);
+}
+
+const getNumberOfAll = (group) => {
+	const numPeople = group.length;
+	const qs = {};
+	group.forEach((person) => person.split("").forEach((q) => {
+		qs[q] = qs[q] ? qs[q] + 1 : 1
+	}));
+	return Object.entries(qs).filter((q) => q[1] === numPeople).length;
 }
 
 module.exports = {
--- day6/customs.spec.js
+++ day6/customs.spec.js
@@ -38,4 +38,38 @@ b`.split("\n\n"))).toEqual(11);
 
   });
 
+  describe('Part 2', () => {
+
+    beforeEach(() => {
+      tested = customs.part2;
+    });
+
+    it('Returns correct number', () => {
+      expect(tested("abc\n\nab\nac".split("\n\n"))).toEqual(4)
+    });
+
+    it('Ensures dupicates in a group are not counted', () => {
+      expect(tested("abc\nad\n\nab\nac".split("\n\n"))).toEqual(2);
+    });
+
+    it('Works with the test from the site', () => {
+      expect(tested(`abc
+
+a
+b
+c
+
+ab
+ac
+
+a
+a
+a
+a
+
+b`.split("\n\n"))).toEqual(6);
+    })
+
+  });
+
 });
